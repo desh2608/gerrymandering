4d268d9a99900af9dfffe237c61261a062a56139
Desh Raj <r.desh26@gmail.com>
2018-12-02 22:42:21 -0500

    added functions for conflicting edges

--- gerry.R
+++ gerry.R
@@ -42,17 +42,44 @@ score.minority <- function(E){
 }
 
 ## Sample new redistrictings using MH algorithm
-## Input: vector of precinct ids
+## Input: vector of precinct ids, adjacency matrix
 ## Output: generated redistricting samples
 ## section 3.3
-sampleMH <- function(precincts){
+sampleMH <- function(precincts, A){
   
   # step 1
   dist <- rep(NA, 16)
   E <- randomDist(precincts, len(dist))
   
   # step 2
+  list[u, v] <- pickConflictingEdge(precincts, A)
   
+}
+
+## Pick any conflicting edge randomly in graph
+## Conflicting edge means the vertices belong to 
+## different districts, i.e., the edge
+## crosses a district boundary.
+pickConflictingEdge <- function(E, A){
+  edges <- findConflictingEdges(E, A)
+  n <- length(edges)
+  ind <- runif()
+}
+
+## Find all conflicting edges in graph
+## Inputs: E (precinct-wise allocation dict),
+## A (adjacency matrix)
+## Output: list of conflicting edges
+findConflictingEdges <- function(E, A){
+  edges <- which(A==1, arr.ind = TRUE)
+  conflict <- list()
+  j <- 1
+  for (i in 1:length(edges)){
+    if(E[edges[i,1]] != E[edges[i,2]]){
+      conflict[[j]] <- edges[i]
+      j <- j+1
+    }
+  }
 }
 
 ## Create random districts from given precincts
@@ -62,37 +89,15 @@ randomDist <- function(precincts, num_dist){
   return (rDist)
 }
 
-## Transition kernel Q for E to E.new
-## section 3.3
-Q <- function(E){
-  
-}
-
-## Function to preprocess data fields
-preprocess <- function(state){
-  data <- cbind(c(as.numeric(state@data[["id"]])),
-                c(state@data[["PREC_EL"]]),
-                c(state@data[["MET_ARE"]]),
-                c(state@data[["PRES_DEM16"]]),
-                c(state@data[["PRES_IN"]]),
-                c(state@data[["PRES_GRN16"]]),
-                c(state@data[["PRES_REP16"]]))
-  
-  # remove precincts with <NA> districts
-  data.new <- subset(data, data[ , 3] != "<NA>")
-
-  return(data.new)
-}
-
 ## Count number of conflicted edges in a 
 ## redistricting
-conflicted <- function(E, connected){
+conflicted <- function(D, adj){
   total <- 0
-  for (i in 1:length(E)){
-    precincts <- E[i]
-    for (j in 1:(length(precincts)-1)){
-      for (k in j+1:length(precincts)){
-        if (connected[]==0){
+  for (i in 1:length(D)){
+    district <- D[i]
+    for (j in 1:(length(district)-1)){
+      for (k in j+1:length(district)){
+        if (adj[j,k]==0){
           conflicted <- conflicted + 1
         }
       }
@@ -128,6 +133,38 @@ findConnectedComponents <- function(A){
   return (C)
 }
 
+## Function to preprocess data fields
+preprocess <- function(state){
+  data <- cbind(c(as.numeric(state@data[["id"]])+1),
+                c(state@data[["PREC_EL"]]),
+                c(state@data[["MET_ARE"]]),
+                c(state@data[["PRES_DEM16"]]),
+                c(state@data[["PRES_IN"]]),
+                c(state@data[["PRES_GRN16"]]),
+                c(state@data[["PRES_REP16"]]))
+  
+  # remove precincts with <NA> districts
+  data.df <- data.frame(data)
+  data.new <- as.matrix(data.df[complete.cases(data.df),])
+  
+  return(data.new)
+}
+
+## Functions to get redistricting
+getRedistrictingByDistrict <- function(data){
+  districts <- unique(data[,2])
+  n <- length(districts)
+  p <- list()
+  for (i in 1:length(districts)){
+    p[[i]] <- subset(data[,1], data[,2]==districts[i])
+  }
+  return (p)
+}
+
+getRedistrictingByPrecinct <- function(data){
+  return (list(data[,2]))
+}
+
 ##----------------------------------------------------------------
 ## DRIVER CODE
 
@@ -144,3 +181,6 @@ C <- findConnectedComponents(A)
 ## remove NA rows from data
 data <- preprocess(ohio)
 
+## initial redistricting
+E <- getRedistrictingByPrecinct(data)
+D <- getRedistrictingByDistrict(data)
